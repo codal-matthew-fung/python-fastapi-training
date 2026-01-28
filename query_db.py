063db26b92862e80acc52342626e921de6bcce62
codal-matthew-fung <mfung@codal.com>
2026-01-28 11:40:31 +0000

    CIT-1063 Add query db script

---
+++ query_db.py
@@ -0,0 +1,15 @@
+import sqlite3
+import pandas as pd
+
+
+def query_db(query="SELECT * FROM products LIMIT 5;"):
+    query = input("Enter your SQL query: ")
+    conn = sqlite3.connect("books.db")
+    df = pd.read_sql_query(query, conn)
+    print(df)
+    conn.close()
+    return df
+
+
+if __name__ == "__main__":
+    query_db()
